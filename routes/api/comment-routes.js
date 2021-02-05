6766da690e3ca8532261a605073e9182a61f55fe
Lucas Barros <lucasmello.barros@gmail.com>
2021-02-05 12:07:10 -0500

    add ability to reply to comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,12 +2,16 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
-// /api/comments/<pizzaId>
+// /api/comments/<pizzaId>f
 router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").delete(removeComment).put(addReply);
+// remove reply
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
